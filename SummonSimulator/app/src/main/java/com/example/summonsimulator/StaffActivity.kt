78be316ac3f2984a523e5424c072bc909fdf68d4
Andy95721 <linhuien3913@gmail.com>
2025-12-01 11:27:05 +0800

    開發名單更星

--- SummonSimulator/app/src/main/java/com/example/summonsimulator/StaffActivity.kt
+++ SummonSimulator/app/src/main/java/com/example/summonsimulator/StaffActivity.kt
@@ -1,20 +1,33 @@
 package com.example.summonsimulator
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class StaffActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_staff)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+// TextView：組員名單
+        val member1: TextView = findViewById(R.id.tvMember1)
+        val member2: TextView = findViewById(R.id.tvMember2)
+        val member3: TextView = findViewById(R.id.tvMember3)
+        val member4: TextView = findViewById(R.id.tvMember4)
+
+        member1.text = "趙宥程"
+        member2.text = "趙健羽"
+        member3.text = "翁帆侑"
+        member4.text = "林暉恩"
+
+// 返回主頁
+        val btnBack: Button = findViewById(R.id.btnBack)
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
